5aa6b580745a5629467721a860fab13dd01c9aa1
Nagasudhir <nagasudhirpulla@gmail.com>
2026-02-21 17:18:58 +0530

    started web layer, refers #7

---
+++ Web/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using App.Common.Interfaces;
+using Web.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddWebServices(this IHostApplicationBuilder builder)
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        }
+
+        builder.Services.AddScoped<IUser, CurrentUser>();
+
+        builder.Services.AddHttpContextAccessor();
+
+        builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents();
+
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -2,9 +2,6 @@ using Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
